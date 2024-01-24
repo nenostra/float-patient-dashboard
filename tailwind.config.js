2998d20a6ea0c7ab9800717378bb68a6fdabe42c
Cesar Gutierrez <gutierrez.cesar@belonghome.com>
2024-01-23 18:51:15 -0800

    chore: install dependencies and tweek configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: "1rem",
+        center: true,
+      },
+    },
   },
   plugins: [],
 }
